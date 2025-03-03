84f8c10532ba255337b83881b5cf831bdc8c31c8
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-03-02 19:20:14 -0500

    Implemented Playlist Renaming

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import TrackList from "../TrackList/TrackList";
 
 const Playlist = ( {playlistName, tracks, onNameChange, onSave, onRemove} ) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [tempName, setTempName] = useState(playlistName);
 
+    // handle enter edit mode
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleChange = (e) => {
+        setTempName(e.target.value);
+    }
+
+    const handleBlur = () => {
+            onNameChange(tempName);
+            setIsEditing(false);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            onNameChange(tempName);
+            setIsEditing(false);
+        }
+    }
 
     return (
         <div className="Playlist">
+            {isEditing ? (
             <input
                 type="text"
-                value={playlistName}
-                onChange={(e) => onNameChange(e.target.value)}
+                value={tempName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                onKeyDown={handleKeyDown}
+                autoFocus
                 placeholder="Enter Playlist Name"
             />
+                ) : (
+                    <h2 onClick={handleEditClick}>{playlistName || "Click to edit name"}</h2> )}
             <TrackList tracks={tracks} onRemove={onRemove} isRemoval={true}/>
             <button onClick={onSave}>Save Playlist</button>
         </div>
